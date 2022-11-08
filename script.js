947fdac5beb66e940946cb5e69dba784827586af
viktoriiasukhova <111765139+viktoriiasukhova@users.noreply.github.com>
2022-11-08 18:54:47 +0000

    Update script.js

--- script.js
+++ script.js
@@ -85,3 +85,27 @@ function showSlides(n) {
     }
     slides[slideIndex - 1].style.display = "block";
 }
+
+function lock (orientation) {
+    // (A1) GO INTO FULL SCREEN FIRST
+    let de = document.documentElement;
+    if (de.requestFullscreen) { de.requestFullscreen(); }
+    else if (de.mozRequestFullScreen) { de.mozRequestFullScreen(); }
+    else if (de.webkitRequestFullscreen) { de.webkitRequestFullscreen(); }
+    else if (de.msRequestFullscreen) { de.msRequestFullscreen(); }
+  
+    // (A2) THEN LOCK ORIENTATION
+    screen.orientation.lock(orientation);
+  }
+
+  // (B) UNLOCK SCREEN ORIENTATION
+function unlock () {
+    // (B1) UNLOCK FIRST
+    screen.orientation.unlock();
+  
+    // (B2) THEN EXIT FULL SCREEN
+    if (document.exitFullscreen) { document.exitFullscreen(); }
+    else if (document.webkitExitFullscreen) { document.webkitExitFullscreen(); }
+    else if (document.mozCancelFullScreen) { document.mozCancelFullScreen(); }
+    else if (document.msExitFullscreen) { document.msExitFullscreen(); }
+  }
